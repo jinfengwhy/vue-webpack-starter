0bca57db5b61dc57f60d9ce374528ea26e60f5a4
xingzhe <xingzhe290@foxmail.com>
2020-09-11 13:39:58 +0800

    拆分 Navbar 和 Aside 为单个 vue 文件

--- src/router.js
+++ src/router.js
@@ -1,16 +1,6 @@
 import VueRouter from 'vue-router'
-import Foo from './components/Foo.vue'
-import Bar from './components/Bar.vue'
 
 export default new VueRouter({
     routes: [
-        {
-            path: '/foo',
-            component: Foo
-        },
-        {
-            path: '/bar',
-            component: Bar
-        },
     ]
 })
